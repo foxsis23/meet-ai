ad5e64a17fef55889de7990923ba75b97223f4d8
foxsis23 <firz250@gmail.com>
2025-06-23 18:47:54 +0300

    setup better-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,46 @@
-import {Button} from "@/components/ui/button";
+'use client'
 
+import {Button} from "@/components/ui/button";
+import {useState} from "react";
+import {Input} from "@/components/ui/input";
+import {authClient} from "@/lib/auth-client";
 
 export default function Home() {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const {data:session} = authClient.useSession()
+
+    const onSubmit = () =>{
+        authClient.signUp.email({
+            email,
+            name,
+            password
+        },{
+            onError:() =>{
+                window.alert("error")
+            }
+        })
+    }
+
+    if(session){
+       return (
+           <div>
+               <p>Logged in as {session.user.name}</p>
+               <Button onClick={() => authClient.signOut()}>Sign out</Button>
+           </div>
+       )
+    }
+
   return (
-      <Button variant="destructive">
-          asd
-      </Button>
+      <div className="p-4 flex flex-col gap-4">
+          <Input placeholder="name" onChange={(e) => setName(e.target.value)} value={name} />
+          <Input placeholder="email" type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+          <Input placeholder="password" type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+          <Button onClick={onSubmit}>
+              Create user
+          </Button>
+      </div>
   );
 }
